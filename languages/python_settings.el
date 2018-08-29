6beb32944d6440d8433b7da453a234c40cbc45b2
biniu <dawid.binkowski93@gmail.com>
2018-08-29 17:45:15 +0200

    clean up languages

--- languages/python_settings.el
+++
@@ -1,9 +0,0 @@
-
-(use-package anaconda-mode
-:ensure t
-:config
-    (add-hook 'python-mode-hook 'anaconda-mode)
-    (add-hook 'python-mode-hook 'anaconda-eldoc-mode)
-  )
-
-(provide 'python_settings)
