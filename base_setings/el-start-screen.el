0bcb0700b0343386df78cabfb9c3bc9133d29fb0
biniu <dawid.binkowski93@gmail.com>
2018-09-05 05:03:46 +0200

    show agenda on start screen

--- base_setings/el-start-screen.el
+++ base_setings/el-start-screen.el
@@ -18,6 +18,7 @@
   (setq dashboard-items '((recents  . 5)
                     (bookmarks . 5)
                     (projects . 5)
+                    (agenda . 5)
                     (registers . 5)))
   )
 
