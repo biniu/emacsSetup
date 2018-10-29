d7f55854b9aeb23edd71a848d95ceb757bb986df
biniu <dawid.binkowski93@gmail.com>
2018-10-29 16:11:08 +0100

    add python packages

--- languages/el-python.el
+++ languages/el-python.el
@@ -8,4 +8,15 @@
 (use-package flycheck-pyflakes
   :ensure t)
 
+(use-package pyenv-mode
+  :ensure t)
+
+(use-package importmagic
+    :ensure t
+    :config
+    (add-hook 'python-mode-hook 'importmagic-mode))
+
+(when (executable-find "ipython")
+  (setq python-shell-interpreter "ipython"))
+
 (provide 'el-python)
