2f8a9b52d7f8d4abe446d5ccf54c41502476fe0c
binkowskid <dawid.binkowski@ts.fujitsu.com>
2019-02-11 11:29:11 +0100

    add pomodoro package

--- init.el
+++ init.el
@@ -24,3 +24,20 @@
 (require 'el-base-setings)
 (require 'el-languages)
 (require 'el-custom-elisp)
+
+(server-start)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (org-pomodoro yasnippet-snippets which-key use-package try toc-org telephone-line sphinx-doc rotate rainbow-mode pylint pyenv-mode py-autopep8 pocket-reader pandoc origami org-super-agenda org-gcal org-fancy-priorities org-dashboard org-bullets neotree md4rd magit json-mode importmagic hydra hungry-delete helm-system-packages helm-rtags helm-projectile helm-mt helm-gitignore groovy-mode google-translate flycheck-pyflakes flycheck-inline fill-column-indicator dumb-jump doom-themes dashboard counsel cmake-ide clang-format calfw-org calfw buffer-move beacon auto-highlight-symbol ace-window ac-helm))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0)))))
--- languages/el-org.el
+++ languages/el-org.el
@@ -259,5 +259,29 @@ do not already have one."
                            )
       )
 
+(use-package org-pomodoro
+  :ensure t
+  :commands (org-pomodoro)
+  :config
+  (setq alert-user-configuration (quote ((((:category . "org-pomodoro")) libnotify nil)))))
+
+(defun my/org-pomodoro-text-time ()
+  "Return status info about org-pomodoro and if org-pomodoro is not running, try to print info about org-clock.
+    If either org-pomodoro or org-clock aren't active, print \"No Active Task \" "
+  (interactive)
+  (cond ((equal :none org-pomodoro-state)
+         (if (org-clock-is-active)
+             (format "Clocked task: %d minutes - %s"
+                     (org-clock-get-clocked-time) (substring-no-properties org-clock-heading)
+                     "No Active task")))
+        ((equal :pomodoro org-pomodoro-state)
+         (format "%d - Pomodoro: %d minutes - %s"
+                 org-pomodoro-count (/ (org-pomodoro-remaining-seconds) 60) (substring-no-properties org-clock-heading)))
+        ((equal :short-break org-pomodoro-state) "Short Break")
+        ((equal :long-break org-pomodoro-state)  "Long Break")))
+
+;; cmd
+;; emacsclient -e '(my/org-pomodoro-text-time)'
+
 (provide 'el-org)
 ;;; el-org ends here
