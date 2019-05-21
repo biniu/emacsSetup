6a84e456511867027a2a3e5c14d278ad45c20e2c
biniu <dawid.binkowsk93@gmail.com>
2019-05-21 08:36:52 +0200

    add helm buffer cmd

--- base_setings/el-helm.el
+++ base_setings/el-helm.el
@@ -7,6 +7,7 @@
   (global-set-key (kbd "M-x") 'helm-M-x)
   (global-set-key (kbd "C-x r b") #'helm-filtered-bookmarks)
   (global-set-key (kbd "C-x C-f") #'helm-find-files)
+  (global-set-key (kbd "C-x C-b") #'helm-buffers-list)
   )
 
 (use-package ac-helm
