0c7141f89423146b9cd870ec105ab901a6491284
biniu <dawid.binkowski93@gmail.com>
2019-06-05 07:23:58 +0200

    set dracula theme

--- base_setings/el-appearance.el
+++ base_setings/el-appearance.el
@@ -115,25 +115,28 @@
   :ensure t)
 
 ;; Global settings (defaults)
-(setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
-      doom-themes-enable-italic t) ; if nil, italics is universally disabled
+;(setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
+;      doom-themes-enable-italic t) ; if nil, italics is universally disabled
 
 ;; Load the theme (doom-one, doom-molokai, etc); keep in mind that each theme
 ;; may have their own settings.
-(load-theme 'doom-one t)
+;(load-theme 'doom-one t)
 
 ;; Enable flashing mode-line on errors
-(doom-themes-visual-bell-config)
+;(doom-themes-visual-bell-config)
 
 ;; Enable custom neotree theme (all-the-icons must be installed!)
 ;(doom-themes-neotree-config)
 ;; or for treemacs users
-(doom-themes-treemacs-config)
+;(doom-themes-treemacs-config)
 
 ;; Corrects (and improves) org-mode's native fontification.
-(doom-themes-org-config)
+;(doom-themes-org-config)
 
+(use-package dracula-theme
+  :ensure t)
 
+(load-theme 'dracula t)
 
 ;; show white sapces
 ;;
